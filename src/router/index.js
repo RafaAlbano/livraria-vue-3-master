0d2c2409d3a1b9f1c18d5ee9983da8d4298c812c
RafaAlbano <rafaelaalbano70@gmail.com>
2023-06-02 11:07:25 -0300

    commit

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import CategoriaView from "../views/CategoriaView.vue";
+import AutorView from "../views/AutorView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/categorias",
+      name: "categorias",
+      component: CategoriaView,
+    },
+    {
+      path: "/autores",
+      nome: "autores",
+      component: AutorView,
+    },
+  ],
+});
+
+export default router;
